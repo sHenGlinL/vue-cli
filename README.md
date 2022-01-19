63d6a9c9659125558718ec87513bd9b362d595e1
liangshenglin <641579182@qq.com>
2022-01-19 15:29:25 +0800

    init: 仓库初始化

---
+++ README.md
@@ -0,0 +1,1 @@
+# A VUE-CLI FOR MYSELF
---
+++ bin/cli.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+const program = require('commander')
+const chalk = require('chalk')
+
+program
+   // 配置版本号信息
+  .version(`v${require('../package.json').version}`)
+  .usage('<command> [option]')
+
+program
+  .on('--help', () => {
+    // 新增说明信息
+    console.log(`\r\nRun ${chalk.cyan(`cci-yingji <command> --help`)} show details\r\n`)
+  })
+
+program
+  // 定义命令和参数
+  .command('create <app-name>')
+  .description('create a new project')
+  // -f or --force 为强制创建，如果创建的目录存在则直接覆盖
+  .option('-f, --force', 'overwrite target directory if it exist')
+  .action(require("../lib/create"))
+  
+// 解析用户执行命令传入参数
+program.parse(process.argv);
